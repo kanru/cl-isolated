2b044bf71879bb98f12b4347cbd71251c7714e86
Teemu Likonen <tlikonen@iki.fi>
2013-04-18 20:09:29 +0300

    Update copyright years: 2012-2013

--- sandbox-cl.lisp
+++ sandbox-cl.lisp
@@ -1,6 +1,6 @@
 ;;;; Eval-bot --- An IRC bot for CL eval and help
 
-;; Copyright (C) 2012 Teemu Likonen <tlikonen@iki.fi>
+;; Copyright (C) 2012-2013 Teemu Likonen <tlikonen@iki.fi>
 ;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU Affero General Public License as
